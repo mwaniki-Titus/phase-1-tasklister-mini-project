3383eb7dd1b2c6d873046892ea8f54600a109eb4
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-24 17:27:56 +0300

    completed assingment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ // your code here
+ let form = document.querySelector('form')
+ form.addEventListener('submit',(event) => {
+  event.preventDefault()
+  handleToDo(event.target.network.value)
+  form.reset()
+ })
+})
+
+function handleToDo(todo) {
+  let task = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', (event) =>
+  event.target.parentNode.remove()
+  )
+  btn.textContent= 'X'
+  task.textContent = ` ${todo}`
+  TransformStream.textContent (btn)
+  document.querySelector('#tasks').appendChild(task)
+  
+}
